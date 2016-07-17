c15ab184869da1f22655071bf6336a881bcbcfe7
Walid AMMOU <wammou@norsys.fr>
2016-07-17 16:28:20 +0100

    Add others utility methods

--- src/main/java/org/helenium/utils/StringUtils.java
+++ src/main/java/org/helenium/utils/StringUtils.java
@@ -19,6 +19,10 @@
 
 package org.helenium.utils;
 
+import java.text.Normalizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author Walid Ammou < w.ammou@helenium.org >
  */
@@ -26,12 +30,19 @@ package org.helenium.utils;
 public class StringUtils {
 	
 	/**
+	 * Utility class, do not instaciate
+	 */
+	private StringUtils(){
+
+	}
+	
+	/**
 	 * Check if a string is null
 	 * 
 	 * @param pString String to check is null
 	 * @return true if null, false else
 	 */
-	public boolean isNull(final String pString){
+	public static boolean isNull(final String pString){
 		
 		return pString == null;
 	}
@@ -42,19 +53,19 @@ public class StringUtils {
 	 * @param pString String to check is not null
 	 * @return true if not null, false else
 	 */
-	public boolean isNotNull(final String pString){
+	public static boolean isNotNull(final String pString){
 		
 		return !isNotNull(pString);
 	}
 	
 	/**
-	 * Check if a string is not null and not empty
+	 * Check if a string is not null and empty
 	 * 
 	 * @param pString String to check
-	 * @param pTrim if true, the string will be trimmed before performing the check of empty. If false, it well check if not null and not empty
-	 * @return 
+	 * @param pTrim if true, the string will be trimmed before performing the check of empty. If false, it well check if not null and empty
+	 * @return true if empty, false else
 	 */
-	public boolean isNotEmpty(final String pString, final boolean pTrim){
+	public static boolean isEmpty(final String pString, final boolean pTrim){
 		
 		if(pTrim){
 			return isNotNull(pString) && !pString.trim().isEmpty();
@@ -63,6 +74,118 @@ public class StringUtils {
 		return isNotNull(pString) && !pString.isEmpty();
 	}
 	
+	/**
+	 * Check if a string is not null and not empty
+	 * 
+	 * @param pString String to check
+	 * @param pTrim if true, the string will be trimmed before performing the check of empty. If false, it well check if not null and not empty
+	 * @return true if not empty, false else
+	 */
+	public static boolean isNotEmpty(final String pString, final boolean pTrim){
+		
+		return !isEmpty(pString, pTrim);
+	}
 	
+	/**
+	 *  Count the number of occurrences of a character in the string
+	 *  
+	 * @param pString String to loop
+	 * @param pCharacter Character to which count number of occurrences
+	 * @return number of occurrences in the string
+	 */
+	public static int countOccurrences(String pString, char pCharacter){
+		
+		int count = 0;
+	    for (int i=0; i < pString.length(); i++){
+	        if (pString.charAt(i) == pCharacter){
+	             count++;
+	        }
+	    }
+	    return count;
+	}
+	
+	/**
+	 * Reverse the order of string characters
+	 * 
+	 * @param pString String to reverse
+	 * @return reversed string
+	 */
+	public static String reverse(final String pString){
+		
+		return new StringBuffer(pString).reverse().toString();
+	}
+	
+	/**
+     * Extract a sub-string via a regular expression.
+     * 
+     * @param pString String from which extract the sub-string
+     * @param pRegExp Regular expression
+     * @return Sub-string corresponding to the regular expression
+     */
+    public static String extract(final String pString, final String pRegExp) {
+        final Pattern pattern = Pattern.compile(pRegExp);
+        final Matcher matcher = pattern.matcher(pString);
+        String result;
+        if (matcher.find()) {
+            result = matcher.group();
+        } else {
+            result = "";
+        }
+        return result;
+    }
+    
+    /**
+     * Transform a string that may contains Accents to another version without Accents
+     * 
+     * @param pString String to transform
+     * @return Transformed string
+     **/
+    public static String withoutAccent(final String pString) {
+    	
+        return Normalizer.normalize(pString, Normalizer.Form.NFD).replaceAll("[\u0300-\u036F]", "");
+    }
+    
+    /**
+     *  truncation a string to create a string with fixed length
+     * 
+     * @param pString String to truncate
+     * @param pLength Length of resulting string
+     * @param pCharacter Character of truncation
+     * @return String truncated string
+     */
+    public static String truncate(final String pString, final int pLength, final char pCharacter) {
+
+        // If the string is null or empty
+        if (pString == null || pString.isEmpty()) {
+            final StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < pLength; i++) {
+                sb.append(pCharacter);
+            }
+            return sb.toString();
+        }
+        // If the string's length is lower than expected
+        if (pString.length() < pLength) {
+            final StringBuffer sb = new StringBuffer();
+            sb.append(pString);
+            int sbLength;
+            for (sbLength = sb.length(); sbLength <= pLength; sbLength++) {
+                sb.append(pCharacter);
+            }
+            return sb.toString();
+        }
+        // If the string's length is equal to what expected
+        if (pString.length() == pLength) {
+            final StringBuffer sb = new StringBuffer();
+            sb.append(pString);
+            return sb.toString();
+        } else {
+            // If the string's length is higher than what expected
+            final StringBuffer sb = new StringBuffer();
+            final String truncatedString = pString.substring(0, pLength);
+            sb.append(truncatedString);
+            return sb.toString();
+        }
+    }
+
 
 }
